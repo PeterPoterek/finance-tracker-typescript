f4c5c3da9b9f670793d50dd237b56ed8911b467b
Piotr Poterek <peterpoterek0@gmail.com>
2024-05-26 01:29:46 +0200

    clerk auth setup

--- client/src/pages/auth/Auth.tsx
+++ client/src/pages/auth/Auth.tsx
@@ -1,5 +1,18 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, SignOutButton, UserButton } from "@clerk/clerk-react";
+
 const Auth = () => {
-  return <div>Auth</div>;
+  return (
+    <div>
+      <SignedOut>
+        <SignUpButton mode="modal" />
+        <SignInButton mode="modal" />
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default Auth;
