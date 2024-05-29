367a2f14c2e0d256fcad42407f62d51940a3cd4e
Piotr Poterek <86746994+PeterPoterek@users.noreply.github.com>
2024-05-30 01:13:28 +0200

    DatePicker form input

--- client/src/pages/dashboard/FinancialForm/FinancialForm.tsx
+++ client/src/pages/dashboard/FinancialForm/FinancialForm.tsx
@@ -21,12 +21,24 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
+
 //#endregion
 
 import {
   financialSchema,
   FinancialEntry,
 } from "@/services/schemas/formSchemas";
+
+import { CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
+import { cn } from "@/lib/utils";
 
 const FinancialForm = () => {
   const onSubmit = (values: FinancialEntry) => {
@@ -41,6 +53,7 @@ const FinancialForm = () => {
       description: "",
       amount: 0,
       transactionCategory: undefined,
+      createdAt: undefined,
     },
   });
 
@@ -64,7 +77,6 @@ const FinancialForm = () => {
               </FormItem>
             )}
           />
-
           <FormField
             control={form.control}
             name="amount"
@@ -107,6 +119,51 @@ const FinancialForm = () => {
                 </Select>
                 <FormDescription>
                   Choose a category to classify this transaction.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="createdAt"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Date of Transaction</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[240px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                      }
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormDescription>
+                  The date when this transaction was made.
                 </FormDescription>
                 <FormMessage />
               </FormItem>
--- client/src/services/schemas/formSchemas.ts
+++ client/src/services/schemas/formSchemas.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
 export const financialSchema = z.object({
-  id: z.string(),
+  // id: z.string(),
   description: z.string().min(1, "Required"),
   amount: z.number().min(1, "Required"),
 
   transactionCategory: z.enum(["income", "expense"]),
-  createdAt: z.string(),
+  createdAt: z.date(),
 });
 
 export type FinancialEntry = z.infer<typeof financialSchema>;
