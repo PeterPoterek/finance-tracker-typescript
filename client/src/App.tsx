afe4a88c92fd1c34093c0713b6539e92c7ca1c49
Piotr Poterek <86746994+PeterPoterek@users.noreply.github.com>
2024-05-30 23:48:00 +0200

    not found page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Auth from "./pages/auth/Auth";
 import Navbar from "./pages/nav/Navbar";
+import ErrorPage from "./pages/error/ErrorPage";
 
 import { ThemeProvider } from "./components/ui/theme-provider";
 
@@ -15,6 +16,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Dashboard />}></Route>
             <Route path="/auth" element={<Auth />}></Route>
+            <Route path="*" element={<ErrorPage />} />
           </Routes>
         </div>
       </Router>
