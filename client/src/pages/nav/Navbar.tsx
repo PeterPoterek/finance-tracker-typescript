743a8c32568289c3a9c6ce8501e0a6ab0747261f
Piotr Poterek <86746994+PeterPoterek@users.noreply.github.com>
2024-05-31 00:50:21 +0200

    theme persist

--- client/src/pages/nav/Navbar.tsx
+++ client/src/pages/nav/Navbar.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   AlertDialog,
@@ -17,11 +16,15 @@ import { Switch } from "@/components/ui/switch";
 
 import { useTheme } from "@/components/ui/theme-provider";
 
-type Theme = "light" | "dark";
+type Theme = "light" | "dark" | "system";
 
 const Navbar = () => {
-  const { setTheme } = useTheme();
-  const [currentTheme, setCurrentTheme] = useState<Theme>("light");
+  const { theme, setTheme } = useTheme();
+  const [currentTheme, setCurrentTheme] = useState<Theme>(theme);
+
+  useEffect(() => {
+    setCurrentTheme(theme);
+  }, [theme]);
 
   const toggleTheme = () => {
     const newTheme = currentTheme === "light" ? "dark" : "light";
@@ -31,18 +34,18 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="fixed flex justify-end  w-full m-auto p-2.5 pl-10 pr-10 ">
-        <div className="flex justify-center items-center gap-5 ">
-          <Switch onCheckedChange={toggleTheme} />
-
+      <div className="fixed flex justify-end w-full m-auto p-2.5 pl-10 pr-10">
+        <div className="flex justify-center items-center gap-5">
+          <Switch
+            checked={currentTheme === "dark"}
+            onCheckedChange={toggleTheme}
+          />
           <p>User name</p>
-
           <div className="flex">
             <Avatar>
               <AvatarImage src="https://placehold.co/460x460?text=IMG" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
-
             <AlertDialog>
               <AlertDialogTrigger asChild>
                 <Button variant="linkSecondary" className="text-base">
