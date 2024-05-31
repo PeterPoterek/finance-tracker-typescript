f377d0e56c6029066b66504fba8535743086f881
Piotr Poterek <86746994+PeterPoterek@users.noreply.github.com>
2024-05-31 18:27:20 +0200

    radio input

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,23 @@
 import FinancialForm from "./FinancialForm/FinancialForm";
 import FinancialList from "./FinancialList/FinancialList";
 
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+
 const Dashboard = () => {
   return (
-    <div>
-      <div className="pt-[4rem]">
-        <h1>Welcome !</h1>
-        <p>Add Your Income/Expense</p>
+    <div className="m-auto max-w-5xl">
+      <div className="pt-[4rem] flex justify-center items-center">
+        <RadioGroup className="flex" defaultValue="option-one">
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="option-one" id="option-one" />
+            <Label htmlFor="option-one">Expenses</Label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="option-two" id="option-two" />
+            <Label htmlFor="option-two">Incomes</Label>
+          </div>
+        </RadioGroup>
       </div>
 
       <div className="m-auto max-w-5xl ">
