86caf78a9c50d481ab53627a8c1a8ec95f19b6d9
Piotr Poterek <peterpoterek0@gmail.com>
2024-06-05 05:16:34 +0200

    even more cooler console.logs

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,17 +8,30 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 const dbUrl: string = process.env.DB_HOST || 'database_url';
 
+const dividers = (length: number) => {
+  let dividersCount = '';
+  for (let i = 0; i < length; i++) {
+    dividersCount += '─';
+  }
+  return dividersCount;
+};
+
 const startServer = async () => {
   try {
+    const divider = dividers(50);
     await mongoose.connect(dbUrl);
 
+    console.clear();
+    console.log(divider.black);
     console.log('Database connection successful ✅'.green.bold);
+    console.log();
 
     app.listen(port, () => {
       const url = `http://localhost:${port}`.cyan.dim.underline;
-      console.log('---------------------------------------------------'.black);
+
       console.log(`🐱 Server is up and running at ${url} 🐱`.magenta.bold);
-      console.log('---------------------------------------------------'.black);
+      console.log(divider.black);
+      console.log();
     });
   } catch (err) {
     if (err instanceof Error) {
