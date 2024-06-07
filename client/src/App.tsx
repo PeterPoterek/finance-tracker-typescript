d0270d708798fa5c6504d7df9c9295ea34934f8b
Piotr Poterek <peterpoterek0@gmail.com>
2024-06-07 02:39:37 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,6 @@
-// import { Suspense } from "react";
+import { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import { useSelector } from "react-redux";
-import { RootState } from "./redux/store/store";
-
 import Dashboard from "./pages/dashboard/Dashboard";
 import Auth from "./pages/auth/Auth";
 import Navbar from "./pages/nav/Navbar";
@@ -12,24 +9,30 @@ import LoadingSpinner from "./pages/loading/LoadingSpinner";
 import UserProfile from "./pages/account/UserProfile";
 import RequireAuth from "./pages/protectedRoute/RequireAuth";
 import { ThemeProvider } from "./components/ui/theme-provider";
+import { RootState } from "./redux/store/store";
 
 const App = () => {
   const isLoading = useSelector((state: RootState) => state.loading);
 
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Auth />}></Route>
-          <Route element={<RequireAuth />}>
-            <Route path="/dashboard" element={<Dashboard />}></Route>
-            <Route path="/account" element={<UserProfile />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Route>
-        </Routes>
-      </Router>
-      {isLoading && <LoadingSpinner />}
+      <Suspense fallback={<LoadingSpinner />}>
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Auth />}></Route>
+              <Route element={<RequireAuth />}>
+                <Route path="/dashboard" element={<Dashboard />}></Route>
+                <Route path="/account" element={<UserProfile />} />
+                <Route path="*" element={<ErrorPage />} />
+              </Route>
+            </Routes>
+          </Router>
+        )}
+      </Suspense>
     </ThemeProvider>
   );
 };
