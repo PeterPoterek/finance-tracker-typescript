5d334290fe7265489c35d8fb922df7ccaaf59c16
Piotr Poterek <peterpoterek0@gmail.com>
2024-06-07 03:18:41 +0200

    react lazy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,16 @@
-import { Suspense } from "react";
+import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
-import Dashboard from "./pages/dashboard/Dashboard";
-import Auth from "./pages/auth/Auth";
 import Navbar from "./pages/nav/Navbar";
 import ErrorPage from "./pages/error/ErrorPage";
 import LoadingSpinner from "./pages/loading/LoadingSpinner";
-import UserProfile from "./pages/account/UserProfile";
-import RequireAuth from "./pages/protectedRoute/RequireAuth";
 import { ThemeProvider } from "./components/ui/theme-provider";
 import { RootState } from "./redux/store/store";
+
+const Dashboard = lazy(() => import("./pages/dashboard/Dashboard"));
+const Auth = lazy(() => import("./pages/auth/Auth"));
+const UserProfile = lazy(() => import("./pages/account/UserProfile"));
+const RequireAuth = lazy(() => import("./pages/protectedRoute/RequireAuth"));
 
 const App = () => {
   const isLoading = useSelector((state: RootState) => state.loading);
@@ -23,9 +24,9 @@ const App = () => {
           <Router>
             <Navbar />
             <Routes>
-              <Route path="/" element={<Auth />}></Route>
+              <Route path="/" element={<Auth />} />
               <Route element={<RequireAuth />}>
-                <Route path="/dashboard" element={<Dashboard />}></Route>
+                <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/account" element={<UserProfile />} />
                 <Route path="*" element={<ErrorPage />} />
               </Route>
