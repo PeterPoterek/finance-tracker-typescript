7860fd46f56963e634e1e418d5e406de392d5990
Piotr Poterek <peterpoterek0@gmail.com>
2024-06-11 03:17:02 +0200

    incomes crud

--- server/src/controllers/incomeController.ts
+++ server/src/controllers/incomeController.ts
@@ -56,3 +56,52 @@ export const addIncome = async (req: Request, res: Response) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+export const updateIncome = async (req: Request, res: Response) => {
+  try {
+    const userId = req.user?.userId;
+    const { id } = req.params;
+    const { description, value, category } = req.body;
+
+    if (!userId) {
+      return res.status(401).json({ error: "User ID not found" });
+    }
+
+    const income = await IncomeModel.findOneAndUpdate(
+      { _id: id, user: userId },
+      { description, value, category },
+      { new: true }
+    );
+
+    if (!income) {
+      return res.status(404).json({ error: "Income not found" });
+    }
+
+    res.status(200).json({ message: "Income updated successfully", income });
+  } catch (error) {
+    console.error("Error updating income:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const deleteIncome = async (req: Request, res: Response) => {
+  try {
+    const userId = req.user?.userId;
+    const { id } = req.params;
+
+    if (!userId) {
+      return res.status(401).json({ error: "User ID not found" });
+    }
+
+    const income = await IncomeModel.findOneAndDelete({ _id: id, user: userId });
+
+    if (!income) {
+      return res.status(404).json({ error: "Income not found" });
+    }
+
+    res.status(200).json({ message: "Income deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting income:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- server/src/routes/api/incomeRouter.ts
+++ server/src/routes/api/incomeRouter.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
-import { getAllIncomes, addIncome } from "../../controllers/incomeController";
+import {
+  addIncome,
+  getAllIncomes,
+  updateIncome,
+  deleteIncome,
+} from "../../controllers/incomeController";
 import { verifyJWT } from "../../middleware/verifyJWT";
 
 const incomeRouter = Router();
 
 incomeRouter.get("/", verifyJWT, getAllIncomes);
 incomeRouter.post("/", verifyJWT, addIncome);
+incomeRouter.put("/:id", verifyJWT, updateIncome);
+incomeRouter.delete("/:id", verifyJWT, deleteIncome);
 
 export default incomeRouter;
