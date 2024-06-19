e2b98cb71a97bd1ea1a45d7687c0dd1a6f17b1bc
Piotr Poterek <peterpoterek0@gmail.com>
2024-06-19 23:06:41 +0200

    UserCard moved separate continent

---
+++ client/src/pages/account/UserCard.tsx
@@ -0,0 +1,110 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Separator } from "@/components/ui/separator";
+
+import useUser from "@/hooks/useUser";
+
+const UserCard = () => {
+  const user = useUser();
+
+  return (
+    <Card className="w-full max-w-md">
+      <CardHeader>
+        <div className="flex items-center justify-between w-full">
+          <div>
+            <CardTitle>My Account</CardTitle>
+            <CardDescription>Manage your account information</CardDescription>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <div className="flex items-center space-x-4">
+          <Avatar>
+            <AvatarImage src={user.avatarURL} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+
+          <label className="cursor-pointer text-teal-400">
+            <input type="file" className="hidden" accept="image/*" />
+            Upload Image
+          </label>
+        </div>
+        <Separator />
+
+        <div>
+          <Label>Username</Label>
+          <p className="text-lg font-medium">{user.username}</p>
+        </div>
+        <div>
+          <Label>Email</Label>
+          <p className="text-lg font-medium">{user.email}</p>
+        </div>
+        <Separator />
+      </CardContent>
+      <CardFooter className="flex justify-center gap-5">
+        <Button variant="outline">Forgot password</Button>
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">Edit Profile</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit Profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Username
+                </Label>
+                <Input
+                  id="username"
+                  defaultValue={user.username}
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="email" className="text-right">
+                  Email
+                </Label>
+
+                <Input
+                  id="email"
+                  defaultValue={user.email}
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default UserCard;
--- client/src/pages/account/UserProfile.tsx
+++ client/src/pages/account/UserProfile.tsx
@@ -2,43 +2,22 @@ import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 
 //#region shadcn imports
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Separator } from "@/components/ui/separator";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Label } from "@/components/ui/label";
+
 //#endregion
 
 import { AppDispatch } from "@/redux/store/store";
 import { getCurrentUser } from "@/redux/slices/userSlice";
-import useUser from "@/hooks/useUser";
 // import useAuth from "@/hooks/useAuth";
 import useAxiosPrivate from "@/hooks/useAxiosPrivate";
 import BarChart from "./BarChart";
 import PieChart from "./PieChart";
+import UserCard from "./UserCard";
 
 const UserProfile = () => {
   const dispatch = useDispatch<AppDispatch>();
   const axiosPrivate = useAxiosPrivate();
-  const user = useUser();
   // const auth = useAuth();
 
   useEffect(() => {
@@ -62,86 +41,7 @@ const UserProfile = () => {
   return (
     <div className="pt-[6rem] flex justify-center items-center flex-col gap-5">
       <div>
-        <Card className="w-full max-w-md">
-          <CardHeader>
-            <div className="flex items-center justify-between w-full">
-              <div>
-                <CardTitle>My Account</CardTitle>
-                <CardDescription>
-                  Manage your account information
-                </CardDescription>
-              </div>
-            </div>
-          </CardHeader>
-          <CardContent className="space-y-4">
-            <div className="flex items-center space-x-4">
-              <Avatar>
-                <AvatarImage src={user.avatarURL} />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-
-              <label className="cursor-pointer text-teal-400">
-                <input type="file" className="hidden" accept="image/*" />
-                Upload Image
-              </label>
-            </div>
-            <Separator />
-
-            <div>
-              <Label>Username</Label>
-              <p className="text-lg font-medium">{user.username}</p>
-            </div>
-            <div>
-              <Label>Email</Label>
-              <p className="text-lg font-medium">{user.email}</p>
-            </div>
-            <Separator />
-          </CardContent>
-          <CardFooter className="flex justify-center gap-5">
-            <Button variant="outline">Forgot password</Button>
-
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button variant="outline">Edit Profile</Button>
-              </DialogTrigger>
-              <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                  <DialogTitle>Edit Profile</DialogTitle>
-                  <DialogDescription>
-                    Make changes to your profile here. Click save when you're
-                    done.
-                  </DialogDescription>
-                </DialogHeader>
-                <div className="grid gap-4 py-4">
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="username" className="text-right">
-                      Username
-                    </Label>
-                    <Input
-                      id="username"
-                      defaultValue={user.username}
-                      className="col-span-3"
-                    />
-                  </div>
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="email" className="text-right">
-                      Email
-                    </Label>
-
-                    <Input
-                      id="email"
-                      defaultValue={user.email}
-                      className="col-span-3"
-                    />
-                  </div>
-                </div>
-                <DialogFooter>
-                  <Button type="submit">Save changes</Button>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
-          </CardFooter>
-        </Card>
+        <UserCard />
       </div>
 
       <div className="p-5">
