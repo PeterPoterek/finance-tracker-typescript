e8570275d22c33c6826990d63892defd5c1e8e93
Piotr Poterek <peterpoterek0@gmail.com>
2024-06-27 13:56:29 +0200

    AuthAccordion moved into seperate component

--- client/src/pages/auth/Auth.tsx
+++ client/src/pages/auth/Auth.tsx
@@ -1,110 +1,11 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-
+import AuthAccordion from "./AuthAccordion";
 import AuthForm from "./AuthForm";
 
 const Auth = () => {
   return (
     <div className="max-w-5xl m-auto flex pt-[10rem] gap-5">
       <div className="flex justify-center items-center flex-col p-[5rem] gap-5 w-[56rem] ">
-        <Accordion type="single" collapsible className="w-full h-[653px] ">
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Expense Tracking</AccordionTrigger>
-            <AccordionContent>
-              <p>
-                This finance app allows users to track their expenses. Users can
-                effortlessly categorize expenses, provide detailed descriptions,
-                and specify the amount spent.
-              </p>
-              <p>
-                Furthermore, users gain insights into their spending habits
-                through comprehensive reports and visualizations accessible in
-                the profile tab.
-              </p>
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-2">
-            <AccordionTrigger>User Authentication</AccordionTrigger>
-            <AccordionContent>
-              <p>
-                Users can register, log in, and manage their accounts securely.
-              </p>
-              <p>
-                The app includes a "Forgot Password" functionality, utilizing
-                SendGrid to handle password recovery emails. I have also
-                incorporated industry-standard security measures to protect user
-                data, including encrypted passwords and secure token-based
-                authentication.
-              </p>
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-3">
-            <AccordionTrigger>Technology Stack</AccordionTrigger>
-            <AccordionContent>
-              <p>
-                The frontend of this project is built with{" "}
-                <a
-                  target="_blank"
-                  href="https://react.dev/"
-                  className="font-bold"
-                >
-                  React
-                </a>
-                , utilizing{" "}
-                <a
-                  target="_blank"
-                  href="https://ui.shadcn.com/"
-                  className="font-bold"
-                >
-                  shadcn
-                </a>{" "}
-                components and styled with{" "}
-                <a
-                  target="_blank"
-                  href="https://tailwindcss.com/"
-                  className="font-bold"
-                >
-                  TailwindCSS
-                </a>
-                .
-              </p>
-              <p>
-                On the backend, the application utilizes the{" "}
-                <a
-                  target="_blank"
-                  href="https://expressjs.com/"
-                  className="font-bold"
-                >
-                  Express
-                </a>{" "}
-                server with{" "}
-                <a
-                  target="_blank"
-                  href="https://www.mongodb.com/"
-                  className="font-bold"
-                >
-                  MongoDB
-                </a>{" "}
-                as the database.
-              </p>
-              <p>
-                You can check the source code{" "}
-                <a
-                  href="https://github.com/PeterPoterek/finance-tracker-typescript"
-                  target="_blank"
-                  className="font-bold"
-                >
-                  here
-                </a>
-                .
-              </p>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
+        <AuthAccordion />
       </div>
 
       <div>
